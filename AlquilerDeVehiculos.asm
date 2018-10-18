063b85f60f46413c7f847bebb959332f7d99c576
Kevin Fallas <kevin.fallas.b@gmail.com>
2018-10-18 15:42:35 -0600

    lectura txt

---
+++ AlquilerDeVehiculos.asm
@@ -0,0 +1,41 @@
+.model small
+.stack 100h
+.data
+  filename db 'vehiculo.txt',0
+  handle dw ?
+  fuff db ? ;buffer del archivo text
+
+.code
+  inicio:
+    mov ax,03h ;sirve para limpiar la pantalla
+    int 10h ;sirve para limpiar la pantalla
+    call abrirTXT
+    call leerTXT
+    .exit
+  end inicio
+
+  abrirTXT proc near
+    mov ah,3dh ;intenta abrir el archivo txt
+    lea dx,filename
+    mov al,0 ;define permiso lectura y escritura
+    int 21h
+    mov handle,ax
+    ret
+  abrirTXT endp
+
+
+  leerTXT proc near
+    mov ah,3fh
+    mov bx,handle
+    lea dx,fbuff
+    mov cx,1 ;leer solo un btye
+    int 21h
+    cmp ax,0 ;revisa si leyo 0 bytes, si es 0, fin de archivo
+    jz finTXT ; si leyo 0 bytes, brincar a fin de archivo
+    mov ah,2
+    int 21h
+    jmp abrirTXT ;repite la funcion hasta que se acabe
+
+  finTXT: ret
+  leerTXT endp
+end
