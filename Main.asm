6469f06fdf25f68f5aa86a1ea578b571137156ac
Kevin Fallas <kevin.fallas.b@gmail.com>
2018-11-03 11:10:53 -0600

    pedir datos para nuevo carro

--- Main.asm
+++ Main.asm
@@ -7,6 +7,43 @@
   handle dw ?
   fbuff  db ? ;buffer del archivo text
   contador db 10
+  ;las variables de abajo son utilizadas a la hora de crear un carro nuevo
+  pedirPlaca db 'Por favor digite la placa del carro.','$'
+  placa db '$$$$$$$$'
+  pedirMarca db 'Por favor digite la marca del carro.','$'
+  marca db '$$$$$$$$'
+  pedirModelo db 'Por favor digite el modelo del carro.','$'
+  modelo db '$$$$$$$$'
+  pedirPeso db 'Por favor digite el peso del carro','$'
+  peso db '$$$$$$$$'
+  pedirColor db 'Por favor digite el color del carro.','$'
+  color db '$$$$$$$$'
+  espacios db '     ','$'
+  columna db 3
+
+
+
+pos_cursor macro col		;UN TIPO DE "METODO" QUE RECIBE 2 PARAMETROS
+	mov ah, 02h
+	mov bh, 00
+	mov dh, dh
+	mov dl, col
+	int 10h						;INTERRUPCION DE VIDEO
+  endm
+
+  imp_texto macro texto
+	lea dx, texto
+	mov ah, 09h
+	int 21h
+  endm
+
+  pedir_datos macro valor
+	mov ah,3fh
+  mov bx,00
+  mov cx,7
+  mov dx, offset[valor]
+  int 21h
+  endm
 
 .code
   .startup
@@ -26,6 +63,12 @@
     ret
   abrirTXT endp
 
+  imprespacios:
+  pos_cursor columna
+  mov ah, 09h
+  int 21h
+  add columna,1
+  jmp leerTXT
 
   leerTXT proc near
     mov ah,3fh
@@ -38,7 +81,7 @@
     mov ah,'#'
     mov al,fbuff ;meter byte leido en al
     cmp ah,al    ;revisar si el caracter leido es un #
-    je leerTXT   ;si lo es brincar a leertxt
+    je imprespacios
     mov ah,'@'
     cmp ah,al    ;misma comparacion de arriba
     je loopCarros ;si es final de linea disminuye el contador
@@ -48,8 +91,6 @@
     jmp leerTXT  ;repite la funcion hasta que se acabe
 
   leerTXT endp
-
-
 
   escribirAtxt proc near
   ;mov ah, 40h          ; service for writing to a file
@@ -68,6 +109,7 @@
   salir: .exit
 
   loopCarros:
+  mov columna,3
   sub contador, 1  ;reduce el contador
   mov ah, contador
   mov al, 0
@@ -81,7 +123,6 @@
   int 21h
   ret
   saltoLinea endp
-
 
   limpiarPantalla proc near
   mov ax,03h  ;sirve para limpiar la pantalla
@@ -97,37 +138,30 @@
   jmp esperarTecla
   teclaIncorrecta endp
 
-  jumpaFinTXT:
-  jmp finTXT
-  ret
+
 
   esperarTecla:;metodo que se encicla mientras el programa esta activo
   call saltoLinea
-<<<<<<< HEAD
-  mov ah,0        ;0 en ah dice que recibe la tecla estripada
-  int 16h         ;int 16h es la encargada de controlar el teclado
-  cmp ah,1eh      ;1eh == hexadecimal para A, revisa si la tecla estripada == A
-  je leerAtras    ;si lo es, llamar funcion que lee hacia atras
-  cmp ah,20h      ;20h == hexadecimal para D,revisa si la tecla estripada == D
-=======
   mov ah,0      ;0 en ah dice que recibe la tecla estripada
   int 16h       ; int 16h es la encargada de controlar el teclado
   cmp ah,48h    ; 48h == hexadecimal para flecha abajo, revisa si la tecla estripada == flecha arriba
   je leerAtras; si lo es, llamar funcion que lee hacia atras
   cmp ah,50h    ; 50h == hexadecimal para flecha arriba,revisa si la tecla estripada == flecha abajo
->>>>>>> 39d01f0ca441e57c6fbc929b570f6f180d272063
   je leerAdelante ; si lo es, llamar funcion que lee hacia adelante
   cmp ah,30h      ;30h == hexadecimal para B,revisa si la tecla estripada == B
-  je moverHandleatras ; si lo es, llamar funcion que busca
+  je jumpamoverHandle ; si lo es, llamar funcion que busca
   cmp ah,01h      ;01h == hexadecimal para esc, revisa si la tecla estripada == esc
   je salir        ;si lo es, llamar funcion que termina programa
+  cmp ah,31h      ;31h == hexadecimal para n, resiva si la tecla estripada == n
+  je ingresarCarro
   call teclaIncorrecta ;si llego aqui significa que se presiono una tecla incorrecta
 
 ;final del segmento repetitivo
 
   leerAdelante proc near ; metodo que limpia la pantalla e imprime los 10 proximos carros en el txt
   call limpiarPantalla
-  call leerTXT
+  ;verificar que haya algo que pueda leer, que no este en EOF
+  call jumpaleerTXT
   jmp esperarTecla
   leerAdelante endp
 
@@ -138,6 +172,58 @@
   jmp esperartecla
   leerAtras endp
 
+  jumpaFinTXT:
+  jmp finTXT
+  ret
+  jumpaleerTXT:
+  jmp leerTXT
+  ret
+
+  jumpaMoverHandle:
+  jmp moverHandleatras
+  ret
+
+  LIMPIAR_CARACTER PROC
+	;MOV BH, 00
+  ;mov dh, 01
+	;MOV BL, PALLEN
+	;MOV placa[BX], '$'
+	;RET
+ENDP
+
+  ingresarCarro proc near ;metodo que pide informacion sobre el carro nuevo y la guarda en variables
+  call limpiarPantalla
+  imp_texto pedirPlaca
+  pedir_datos placa
+  call limpiarPantalla
+  imp_texto placa
+  call limpiarPantalla
+  imp_texto pedirMarca
+  pedir_datos marca
+  call limpiarPantalla
+  imp_texto pedirModelo
+  pedir_datos modelo
+  call limpiarPantalla
+  imp_texto pedirPeso
+  pedir_datos peso
+  call limpiarPantalla
+  imp_texto pedirColor
+  pedir_datos color
+  call limpiarPantalla
+  imp_texto placa
+  call saltoLinea
+  imp_texto marca
+  call saltoLinea
+  imp_texto modelo
+  call saltoLinea
+  imp_texto peso
+  call saltoLinea
+  imp_texto color
+  ingresarCarro endp
+
+  segundoJumpaFinTxt:
+  jmp jumpaFinTXT
+  ret
 
   moverHandleatras proc near ;metodo medio complejo, ubica el handle, lo mueve 20 lineas atras como preparacion para imprimir 10 carros
   mov al, 1
@@ -152,8 +238,7 @@
   mov cx,1        ;movi el handle 2 atras, ahora leo uno adelante, efectivamente leyendo la posicion anterior a la que comence
   int 21h
   cmp ax,0        ;revisa si leyo 0 bytes, si es 0, fin de archivo
-  jz jumpaFinTXT       ;si leyo 0 bytes, brincar a fin de archivo
-
+  jz segundojumpaFinTXT       ;si leyo 0 bytes, brincar a fin de archivo
   mov ah,'@'
   mov al,fbuff    ;meter byte leido en al
   cmp ah,al       ;revisar si el caracter leido es un @
